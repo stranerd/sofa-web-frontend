41a93d2ea00d2d49b2f730619c0551bfb52b526b
Ayomide Akinola <drayfocus@gmail.com>
2023-09-30 20:29:28 +0100

    fix: flip card issues

--- apps/website/src/components/quizzes/Flashcard.vue
+++ apps/website/src/components/quizzes/Flashcard.vue
@@ -7,27 +7,33 @@
         class="group h-full lg:!w-[50%] mdlg:!w-[70%] md:!w-[80%] w-full [perspective:1000px] cursor-pointer md:!px-0 px-4"
       >
         <div
+          class="flip-card rounded-xl"
           @click="showAnswer ? (showAnswer = false) : (showAnswer = true)"
-          :class="`relative h-full w-full rounded-xl transition-all duration-500 bg-white [transform-style:preserve-3d]   ${
-            showAnswer ? '[transform:rotateY(180deg)]' : ''
-          } `"
+          @swiped="handleSwiperAction($event)"
           style="box-shadow: 0px 4px 4px rgba(8, 0, 24, 0.05)"
         >
           <div
-            class="absolute inset-0 flex flex-col items-center justify-center px-4"
+            class="flip-card-inner rounded-xl"
+            :style="`${showAnswer ? 'transform: rotateY(180deg);' : ''}`"
           >
-            <sofa-header-text
-              :customClass="'!font-bold md:!text-2xl text-lg'"
-              >{{ questionRef.question }}</sofa-header-text
+            <div class="flip-card-front rounded-xl">
+              <div
+                class="absolute inset-0 flex flex-col items-center justify-center px-4"
+              >
+                <sofa-header-text
+                  :customClass="'!font-bold md:!text-2xl text-lg'"
+                  >{{ questionRef.question }}</sofa-header-text
+                >
+              </div>
+            </div>
+            <div
+              class="flip-card-back rounded-xl flex flex-col items-center justify-center px-12 text-center"
             >
-          </div>
-          <div
-            class="absolute inset-0 h-full w-full rounded-xl bg-white 0 flex flex-col items-center justify-center px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]"
-          >
-            <sofa-header-text
-              :customClass="'!font-semibold md:!text-xl text-base'"
-              >{{ questionRef.answer }}</sofa-header-text
-            >
+              <sofa-header-text
+                :customClass="'!font-semibold md:!text-xl text-base '"
+                >{{ questionRef.answer }}</sofa-header-text
+              >
+            </div>
           </div>
         </div>
       </div>
@@ -44,7 +50,7 @@
 </template>
 <script lang="ts">
 import { QuizQuestion } from "sofa-logic/src/logic/types/domains/study";
-import { defineComponent, onMounted, reactive, ref, toRef, watch } from "vue";
+import { defineComponent, reactive, ref, toRef } from "vue";
 import { SofaHeaderText, SofaNormalText } from "sofa-ui-components";
 
 export default defineComponent({
@@ -74,19 +80,62 @@ export default defineComponent({
       JSON.parse(JSON.stringify(questionRef.value))
     );
 
-    const setAnswer = () => {
-      //   console.log(question);
-    };
+    const handleSwiperAction = (event: any) => {
+      if (event.detail.dir == "left") {
+        showAnswer.value = false;
+      }
 
-    onMounted(() => {
-      setAnswer();
-    });
+      if (event.detail.dir == "right") {
+        showAnswer.value = true;
+      }
+    };
 
     return {
       question,
       showAnswer,
       questionRef,
+      handleSwiperAction,
     };
   },
 });
 </script>
+<style scoped>
+/* The flip card container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
+.flip-card {
+  background-color: transparent;
+  width: 100%;
+  height: 100%;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Position the front and back side */
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: white;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: white;
+  transform: rotateY(180deg);
+}
+</style>
