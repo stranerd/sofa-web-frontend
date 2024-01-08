f19d20a1722d39a677bdb6560ef9613926e0708b
DevAdedeji <adedejitewogbade2@gmail.com>
2024-01-08 12:31:03 +0100

    Done with the tutor help modal

--- libs/sofa-ui-components/src/components/SofaModal2/index.vue
+++ libs/sofa-ui-components/src/components/SofaModal2/index.vue
@@ -2,7 +2,7 @@
 	<component :is="Teleport" to="body">
 		<transition name="fade" appear>
 			<div
-				class="fixed top-0 w-[100dvw] h-[100dvh] z-[1000] bg-black text-white bg-opacity-40 flex flex-col items-center mdlg:justify-start justify-end">
+				:class="`fixed top-0 w-[100dvw] h-[100dvh] z-[1000] bg-black text-white bg-opacity-40 flex flex-col items-center mdlg:justify-start justify-end ${customClass}`">
 				<div class="h-full w-full absolute top-0 left-0" @click="closeModal" />
 				<div
 					class="z-[1] w-full mdlg:w-[50%] max-h-[100%] mdlg:mt-[10%] mdlg:max-h-[80%] overflow-y-auto bg-white text-bodyBlack rounded-t-2xl mdlg:rounded-b-2xl"
@@ -28,6 +28,10 @@ const props = defineProps({
 		type: String,
 		required: false,
 		default: 'mdlg:max-w-[800px]',
+	},
+	customClass: {
+		type: String,
+		default: '',
 	},
 })
 
