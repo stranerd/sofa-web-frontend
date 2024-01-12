3186380d66afcbb67b1d9b88036ddfa471177a86
kevinand11 <kevinfizu@gmail.com>
2024-01-12 23:43:55 +0100

    cleaning up App modals

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -1,21 +1,10 @@
 <template>
-	<div
-		v-if="loaderSetup.loading || loaderSetup.loaders.length"
-		class="fixed top-0 left-0 h-full w-full bg-black opacity-80 z-[100000000000] cursor-not-allowed">
-		<div class="loader-bar" />
-	</div>
 	<metainfo>
 		<template #title="{ content }">
 			{{ content ? `${content} | SOFA` : `SOFA` }}
 		</template>
 	</metainfo>
 	<router-view :key="$route.path" />
-	<SofaAlert
-		v-for="(alert, i) in loaderSetup.alerts"
-		:key="i"
-		:close="() => loaderSetup.alerts.splice(i, 1)"
-		:content="alert.message"
-		:type="alert.type" />
 	<study-mode-modal v-if="showStudyMode" />
 	<!-- Save to folder -->
 	<save-to-folder
@@ -31,43 +20,6 @@
 		:title="`Report this ${reportMaterialSetup.type}`"
 		@on-review-submitted="sendReportMaterial" />
 	<add-material-modal v-if="showAddItem" :close="() => (showAddItem = false)" />
-	<SofaDeletePrompt
-		v-for="confirmation in confirmations"
-		:key="confirmation.id"
-		:title="confirmation.title"
-		:sub-title="confirmation.sub"
-		:close="() => confirmation.close(false)"
-		:buttons="[
-			{
-				label: confirmation.left?.label ?? 'No',
-				hide: confirmation.left?.hide,
-				bgColor: confirmation.left?.bg,
-				textColor: confirmation.left?.color,
-				isClose: true,
-				action: () => confirmation.close(false),
-			},
-			{
-				label: confirmation.right?.label ?? 'Yes',
-				hide: confirmation.right?.hide,
-				bgColor: confirmation.right?.bg,
-				textColor: confirmation.right?.color,
-				isClose: false,
-				action: () => confirmation.close(true),
-			},
-		]" />
-	<SofaSuccessPrompt
-		v-for="confirmation in successes"
-		:key="confirmation.id"
-		:title="confirmation.title"
-		:sub-title="confirmation.sub"
-		:close="() => confirmation.close(false)"
-		:button="{
-			label: confirmation.button?.label ?? 'Ok',
-			hide: confirmation.button?.hide,
-			bgColor: confirmation.button?.bg,
-			textColor: confirmation.button?.color,
-			action: () => confirmation.close(true),
-		}" />
 	<Modals />
 </template>
 
@@ -75,26 +27,20 @@
 import AddMaterialModal from '@/components/common/AddMaterialModal.vue'
 import RateAndReviewModal from '@/components/common/RateAndReviewModal.vue'
 import Modals from '@/components/core/modals/Modals.vue'
-import { UsersUseCases } from '@modules/users'
-import { Logic } from 'sofa-logic'
-import { SofaAlert, SofaDeletePrompt, SofaSuccessPrompt } from 'sofa-ui-components'
-import { watch } from 'vue'
-import { useMeta } from 'vue-meta'
-import { useRoute } from 'vue-router'
 import SaveToFolder from '@/components/study/SaveToFolder.vue'
 import StudyModeModal from '@/components/study/StudyModeModal.vue'
 import { showAddItem } from '@/composables'
 import { useAuth } from '@/composables/auth/auth'
 import { reportMaterialSetup, selectedFolderMaterailToAdd, sendReportMaterial, showStudyMode } from '@/composables/library'
+import { UsersUseCases } from '@modules/users'
+import { watch } from 'vue'
+import { useMeta } from 'vue-meta'
+import { useRoute } from 'vue-router'
 
 useMeta({
-	title: 'Home',
+	title: 'SOFA',
 	htmlAttrs: { lang: 'en', amp: true },
 })
-
-const loaderSetup = Logic.Common.loaderSetup
-const confirmations = Logic.Common.confirmations
-const successes = Logic.Common.successes
 
 const route = useRoute()
 const { user } = useAuth()
--- apps/website/src/components/core/modals/Modals.vue
+++ apps/website/src/components/core/modals/Modals.vue
@@ -13,12 +13,65 @@
 		:close="popovers[key].modalArgs?.closeOnClickOutside ?? true ? () => pClose(key) : null">
 		<component :is="popovers[key].component" v-bind="popovers[key].args ?? {}" :close="() => pClose(key)" />
 	</SofaModal>
+	<SofaDeletePrompt
+		v-for="confirmation in confirmations"
+		:key="confirmation.id"
+		:title="confirmation.title"
+		:sub-title="confirmation.sub"
+		:close="() => confirmation.close(false)"
+		:buttons="[
+			{
+				label: confirmation.left?.label ?? 'No',
+				hide: confirmation.left?.hide,
+				bgColor: confirmation.left?.bg,
+				textColor: confirmation.left?.color,
+				isClose: true,
+				action: () => confirmation.close(false),
+			},
+			{
+				label: confirmation.right?.label ?? 'Yes',
+				hide: confirmation.right?.hide,
+				bgColor: confirmation.right?.bg,
+				textColor: confirmation.right?.color,
+				isClose: false,
+				action: () => confirmation.close(true),
+			},
+		]" />
+	<SofaSuccessPrompt
+		v-for="confirmation in successes"
+		:key="confirmation.id"
+		:title="confirmation.title"
+		:sub-title="confirmation.sub"
+		:close="() => confirmation.close(false)"
+		:button="{
+			label: confirmation.button?.label ?? 'Ok',
+			hide: confirmation.button?.hide,
+			bgColor: confirmation.button?.bg,
+			textColor: confirmation.button?.color,
+			action: () => confirmation.close(true),
+		}" />
+	<SofaAlert
+		v-for="(alert, i) in loaderSetup.alerts"
+		:key="i"
+		:close="() => loaderSetup.alerts.splice(i, 1)"
+		:content="alert.message"
+		:type="alert.type" />
+	<div
+		v-if="loaderSetup.loading || loaderSetup.loaders.length"
+		class="fixed top-0 left-0 h-full w-full bg-black opacity-80 z-[100000000000] cursor-not-allowed">
+		<div class="loader-bar" />
+	</div>
 </template>
 
 <script lang="ts" setup>
 import { modal, popover } from '@/composables/core/modal'
-import { SofaModal2 as SofaModal } from 'sofa-ui-components'
+import { Logic } from 'sofa-logic'
+import { SofaAlert, SofaDeletePrompt, SofaModal2 as SofaModal, SofaSuccessPrompt } from 'sofa-ui-components'
 
 const { modals, stack: mStack, close: mClose } = modal
 const { popovers, stack: pStack, close: pClose } = popover
+
+const loaderSetup = Logic.Common.loaderSetup
+const confirmations = Logic.Common.confirmations
+const successes = Logic.Common.successes
 </script>
