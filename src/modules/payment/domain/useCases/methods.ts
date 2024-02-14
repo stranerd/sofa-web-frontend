1bfd8cacb76ddd7721850bf6160efceade518075
DevAdedeji <adedejitewogbade2@gmail.com>
2024-02-14 12:44:07 +0100

    Done with useMethodsList and useMethod composable

--- src/modules/payment/domain/useCases/methods.ts
+++ src/modules/payment/domain/useCases/methods.ts
@@ -1,11 +1,16 @@
 import { IMethodRepository } from '../irepositories/methods'
-import { QueryParams } from '@modules/core'
+import { MethodEntity } from '../entities/methods'
+import { QueryParams, Listeners } from '@modules/core'
 
 export class MethodsUseCase {
 	repository: IMethodRepository
 
 	constructor(repo: IMethodRepository) {
 		this.repository = repo
+	}
+
+	async getAll() {
+		return await this.repository.get({ all: true })
 	}
 
 	async get(input: QueryParams) {
@@ -16,11 +21,18 @@ export class MethodsUseCase {
 		return await this.repository.find(id)
 	}
 
-	async makePrimary(data: { id: string; userId: string }) {
-		return await this.repository.makePrimary(data.id, data.userId)
+	async makePrimary(id: string) {
+		return await this.repository.makePrimary(id)
 	}
 
-	async delete(data: { id: string; userId: string }) {
-		return await this.repository.delete(data.id, data.userId)
+	async delete(id: string) {
+		return await this.repository.delete(id)
+	}
+	async listenToAll(listener: Listeners<MethodEntity>) {
+		const conditions: QueryParams = {
+			all: true,
+		}
+
+		return await this.repository.listenToMany(conditions, listener, () => true)
 	}
 }
