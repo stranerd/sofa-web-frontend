2ee099280d8b70cb1539d308e9be8df97a814ade
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2024-09-06 21:50:55 +0100

    #added files for marketplace page and started updating, modified layouts.

--- src/application/views/marketplace/index.vue
+++ src/application/views/marketplace/index.vue
@@ -1,29 +1,71 @@
 <template>
 	<ExpandedLayout layoutStyle="mdlg:pb-4">
-		<div class="w-full mdlg:flex hidden flex-col gap-5 pt-8 pb-14 bg-primaryPurple justify-center items-center">
-			<SofaHeaderText color="text-white" size="xl" class="!font-extrabold" content="All contents made by verified creators" />
+		<div
+			class="w-full flex flex-col-reverse mdlg:flex-row gap-5 py-8 mdlg:py-0 mdlg:pt-8 px-10 mdlg:px-20 bg-primaryYellow justify-center items-center mdlg:h-[600px]">
+			<div class="mdlg:w-[70%] h-full flex flex-col justify-center">
+				<div class="w-[80%]">
+					<SofaHeaderText
+						color="text-primaryPurple"
+						size="2xl"
+						class="!font-extrabold"
+						content="All contents made by verified creators" />
 
-			<SofaNormalText color="text-white" customClass="w-[48%] text-center flex items-center justify-center">
-				Everything here is carefully reviewed to ensure the highest quality and accuracy. By purchasing from our marketplace, you
-				can have confidence in the credibility of the creators and the value of the materials.
-			</SofaNormalText>
+					<SofaNormalText color="text-black" customClass="py-4 text-left flex items-center justify-center">
+						Everything here is carefully reviewed to ensure the highest quality and accuracy. By purchasing from our
+						marketplace, you can have confidence in the credibility of the creators and the value of the materials.
+					</SofaNormalText>
+				</div>
 
-			<div class="w-[40%] shadow-custom px-4 py-2 bg-white rounded-custom flex gap-3 items-center justify-between">
-				<form class="flex gap-2 items-center flex-1" @submit.prevent="handleSearch">
-					<SofaIcon name="filter" class="h-[15px]" @click="$router.push('/marketplace/search')" />
-					<SofaNormalText as="router-link" class="pr-2 border-r border-darkLightGray" content="Filter" to="/marketplace/search" />
-					<SofaTextField
-						v-model="searchQuery"
-						class="flex-1"
-						customClass="!border-none w-full"
-						placeholder="Search for anything" />
-				</form>
+				<div class="flex flex-col mdlg:flex-row mdlg:items-center gap-6 w-full">
+					<div class="flex items-center gap-2">
+						<img src="/images/marketplace/creators.png" alt="Creators" />
+						<SofaHeaderText>120+ <span class="font-normal">Creators</span></SofaHeaderText>
+					</div>
+					<div class="flex items-center gap-2">
+						<SofaIcon name="dropbox" class="bg-lightGray rounded-full p-2" />
+						<SofaHeaderText>4500+ <span class="font-normal">Courses</span></SofaHeaderText>
+					</div>
+					<div class="flex items-center gap-2">
+						<SofaIcon name="open-book" class="bg-lightGray rounded-full p-2" />
+						<SofaHeaderText>12000+ <span class="font-normal">Resources</span></SofaHeaderText>
+					</div>
+				</div>
 
-				<SofaIcon name="search-black" class="h-[17px] cursor-pointer w-[20px]" @click="handleSearch" />
+				<div
+					class="w-[80%] shadow-custom px-4 py-2 my-4 bg-white rounded-custom hidden mdlg:flex gap-3 items-center justify-between">
+					<form class="flex gap-2 items-center flex-1" @submit.prevent="handleSearch">
+						<SofaIcon name="search-black" class="h-[17px] cursor-pointer w-[20px]" @click="handleSearch" />
+						<SofaTextField v-model="searchQuery" class="flex-1" customClass="!border-none w-full" placeholder="Search" />
+					</form>
+				</div>
+			</div>
+			<div class="h-full flex justify-center items-center">
+				<SofaImageLoader photoUrl="/images/marketplace/marketplace-hero.png" class="w-full h-full object-contain" />
 			</div>
 		</div>
 
-		<div class="w-full p-4 flex mdlg:hidden flex-col">
+		<div class="w-full flex items-center justify-around py-4">
+			<!-- Icon of select component cannot be changed, also icon fillColor cannot be changed -->
+			<div>
+				<div v-if="$screen.desktop" class="gap-3 flex items-center">
+					<SofaSelect
+						v-for="i in 4"
+						:key="i"
+						:options="[]"
+						placeholder="Subject"
+						class="border-2 rounded-3xl w-[157px] h-[44px] text-grayColor" />
+				</div>
+				<SofaSelect v-else :options="[]" placeholder="Subject" class="border-2 rounded-3xl w-[157px] h-[44px] text-grayColor" />
+			</div>
+			<div class="flex items-center gap-3">
+				<!-- Icon for select component cannot be mutated -->
+				<SofaNormalText size="xl" customClass="font-bold" content="Sort by" />
+				<SofaSelect :options="[]" placeholder="Relevancy" class="border-2 rounded-3xl w-[157px] h-[44px] text-grayColor" />
+			</div>
+		</div>
+
+		<!-- No longer needed -->
+		<!-- <div class="w-full p-4 flex mdlg:hidden flex-col">
 			<form
 				class="w-full shadow-custom px-4 py-2 bg-white rounded-custom flex gap-2 items-center justify-start"
 				@submit.prevent="handleSearch">
@@ -31,19 +73,23 @@
 				<SofaIcon name="search-black" class="h-[15px] cursor-pointer" @click="handleSearch" />
 				<SofaTextField v-model="searchQuery" class="flex-1" customClass="!border-none w-full !px-0" placeholder="Search" />
 			</form>
-		</div>
+		</div> -->
 
-		<div class="mdlg:w-[85%] lg:w-[75%] w-full flex flex-col h-full overflow-y-auto gap-8 mdlg:pl-0 pl-4">
-			<template v-for="material in materials" :key="material.title">
+		<div class="mdlg:w-[85%] lg:w-[75%] w-full flex flex-col h-full gap-8 mdlg:pl-0 pl-4">
+			<template v-for="(material, index) in materials" :key="material.title">
 				<div class="w-full flex flex-col mdlg:gap-4 gap-3">
-					<div class="w-full flex gap-2 pr-4 mdlg:pr-0 items-center justify-between">
-						<SofaNormalText class="!font-bold" :content="material.title" />
-						<SofaNormalText
-							color="text-primaryPink"
-							as="router-link"
-							to="/marketplace/search"
-							class="mdlg:hidden"
-							content="View all" />
+					<div
+						:class="[
+							'w-full flex gap-2 pr-4 p-4 items-center justify-between bg-primaryGreen',
+							index === 0 ? 'bg-primaryYellow' : '',
+							index === 1 ? 'bg-primaryPurple' : '',
+							index === 2 ? 'bg-primaryPink' : '',
+						]">
+						<SofaNormalText class="!font-bold text-white" :content="material.title" />
+						<SofaNormalText color="text-white" as="router-link" to="/marketplace/search" class="inline-flex items-center gap-1">
+							See all
+							<SofaIcon name="alt-arrow-right" class="h-[15px] fill-white" />
+						</SofaNormalText>
 					</div>
 
 					<div
