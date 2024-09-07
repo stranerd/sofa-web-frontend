2908ead557253eba68c9bd65c383dc617e09e982
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2024-09-07 23:42:01 +0100

    #added codes for rest of the page.

--- src/application/views/marketplace/index.vue
+++ src/application/views/marketplace/index.vue
@@ -1,5 +1,18 @@
 <template>
 	<ExpandedLayout layoutStyle="mdlg:pb-4">
+		<form class="w-full flex items-center mdlg:hidden px-4">
+			<div class="w-full flex items-center gap-3 z-50 justify-between bg-lightGray py-4 sticky top-0 left-0">
+				<SofaIcon class="h-[15px]" name="arrow-left" @click="$utils.goBack()" />
+				<SofaHeading content="Marketplace" />
+				<span class="w-4" />
+			</div>
+			<SofaIcon name="search-black" class="h-[15px]" />
+			<!-- <SofaInput v-model="searchQuery" class="flex-1 !bg-white !rounded-custom shadow-custom w-full" placeholder="Search">
+				<template #prefix>
+					<SofaIcon name="search-black" class="h-[15px]" />
+				</template>
+			</SofaInput> -->
+		</form>
 		<div
 			class="w-full flex flex-col-reverse mdlg:flex-row gap-5 py-8 mdlg:py-0 mdlg:pt-8 px-10 mdlg:px-20 bg-primaryYellow justify-center items-center mdlg:h-[600px]">
 			<div class="mdlg:w-[70%] h-full flex flex-col justify-center">
@@ -74,13 +87,12 @@
 				<SofaTextField v-model="searchQuery" class="flex-1" customClass="!border-none w-full !px-0" placeholder="Search" />
 			</form>
 		</div> -->
-
-		<div class="mdlg:w-[85%] lg:w-[75%] w-full flex flex-col h-full gap-8 mdlg:pl-0 pl-4">
+		<div class="mdlg:w-[85%] lg:w-[75%] w-full flex flex-col h-full gap-8 mdlg:pl-0">
 			<template v-for="(material, index) in materials" :key="material.title">
 				<div class="w-full flex flex-col mdlg:gap-4 gap-3">
 					<div
 						:class="[
-							'w-full flex gap-2 pr-4 p-4 items-center justify-between bg-primaryGreen',
+							'w-full flex gap-2 p-4 items-center justify-between bg-primaryGreen',
 							index === 0 ? 'bg-primaryYellow' : '',
 							index === 1 ? 'bg-primaryPurple' : '',
 							index === 2 ? 'bg-primaryPink' : '',
@@ -94,11 +106,34 @@
 
 					<div
 						v-if="material.list.length"
-						class="mdlg:gap-4 flex gap-3 mdlg:p-0 pr-4 flex-nowrap overflow-x-auto scrollbar-hide items-start">
+						class="mdlg:gap-4 flex gap-3 mdlg:p-0 px-4 flex-nowrap overflow-x-auto scrollbar-hide items-start">
+						<!-- <div v-for="m in material.list">
+							{{ m }}
+						</div> -->
 						<StudyMaterialCard v-for="m in material.list" :key="m.id" wrapped :material="m" class="mdlg:w-[20%]" />
 					</div>
 					<div v-else class="pr-4 mdlg:pr-0">
 						<SofaEmptyState :title="material.emptyTitle" :subTitle="material.emptySub" customClass="!h-[230px]" />
+					</div>
+				</div>
+				<!-- Between first and second template-->
+				<div v-if="index === 0" class="w-full flex flex-col py-4">
+					<!-- Your custom content here -->
+					<div class="w-full flex gap-2 px-4 pb-4 items-center justify-between">
+						<SofaNormalText class="!font-bold !text-black" content="Topics recommended for you" />
+						<SofaNormalText
+							color="text-primaryPink"
+							as="router-link"
+							to="/marketplace/search"
+							class="inline-flex items-center gap-1"
+							content="View all" />
+					</div>
+					<div class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 mdlg:p-0 px-4">
+						<SofaNormalText
+							v-for="i in 10"
+							:key="i"
+							content="Organic Chemistry"
+							class="border p-2 border-deepGray mdlg:w-[180px] mdlg:h-[50px] flex justify-center whitespace-nowrap" />
 					</div>
 				</div>
 			</template>
